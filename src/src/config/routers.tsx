c61aa79238ba5d81c547586537eeedaf2aa7dd4f
阳光 <qingxianyang@outlook,com>
2021-09-13 20:46:29 +0800

    删除了一些历史项目

--- src/src/config/routers.tsx
+++ src/src/config/routers.tsx
@@ -1,10 +1,4 @@
-import WordsList from "../page/WordsList";
-import WordEdit from "../page/WordEdit.js";
 import Index from '../page/index'
-import webIndex from '../page/webIndex.js';
-import SentenceList from '../page/SentenceList';
-import SentenceEdit from "../page/SentenceEdit";
-import CheckList from "../page/CheckList";
 
 interface router {
     path: string,
@@ -13,38 +7,6 @@ interface router {
 }
 
 const routers: Array<router> = [
-    {
-        path: "/check/list",
-        component: CheckList
-    },
-    {
-        path: '/sentence/create/:id',
-        component: SentenceEdit
-    },
-    {
-        path: '/sentence/edit/:id',
-        component: SentenceEdit
-    },
-    {
-        path: '/sentence/list',
-        component: SentenceList
-    },
-    {
-        path: "/words/list",
-        component: WordsList
-    },
-    {
-        path: "/words/edit/:id",
-        component: WordEdit
-    },
-    {
-        path: "/words/create/:id",
-        component: WordEdit
-    },
-    {
-        path: '/web',
-        component: webIndex
-    },
     {
         path: "/",
         component: Index
--- src/src/page/index.js
+++ src/src/page/index.js
@@ -1,17 +1,8 @@
 import React from 'react'
 
-class Index extends React.Component {
-    render() {
-        return (
-            <div className={"container"}>
-                <div className={"er_index"}>
-                    <h1>
-                        <a href="./web">网页版入口</a>
-                    </h1>
-
-                </div>
-            </div>
-        )
+class Index extends React.Component{
+    render(){
+        return null;
     }
 }
 
