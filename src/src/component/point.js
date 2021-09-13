aa1006e4422dc041a0e66ee497be829559b2b41e
阳光 <qingxianyang@outlook,com>
2021-09-14 00:03:45 +0800

    基础样式定义完成

--- src/src/component/point.js
+++ src/src/component/point.js
@@ -1,18 +1,33 @@
 import React from 'react'
-import {Card} from "antd";
-
+import {Card,Row,Col} from "antd";
+import {SaveOutlined,OrderedListOutlined} from '@ant-design/icons';
 class Point extends React.Component{
     constructor(props) {
         super(props);
         this.state={
-            pid:props.pid
+            pid:props.pid,
+            id:props.id
         }
     }
     render(){
         return(
-            <Card.Grid>
-
-            </Card.Grid>
+            <Card>
+                <Row justify="space-around" align="middle">
+                    <Col span={20}>
+                        <input
+                            style={{width:"90%",margin:"5px"}}
+                        />
+                    </Col>
+                    <Col span={2}>
+                        <SaveOutlined
+                        />
+                    </Col>
+                    <Col span={2}>
+                        <OrderedListOutlined
+                        />
+                    </Col>
+                </Row>
+            </Card>
         )
     }
 }
--- src/src/page/Collector.js
+++ src/src/page/Collector.js
@@ -1,14 +1,104 @@
 import React from 'react'
+import Point from "../component/point";
+import {Card,Select,Row,Col} from "antd";
+import "./../css/Collector.css"
+import {SaveOutlined,PlusCircleOutlined} from '@ant-design/icons';
+
+const { Option } = Select;
+
+class StatusChange extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            pid:props.id,
+            status:props.status,
+            statusMap:[
+                'new',
+                'solved',
+                'give up'
+            ]
+        }
+    }
+    render() {
+        return(
+            <Select>
+                {
+                    this.state.statusMap.map((Item)=>{
+                        return(
+                            <Option key={Item} value={Item}>{Item}</Option>
+                        )
+                    })
+                }
+            </Select>
+        )
+    }
+}
 
 class Collector extends React.Component{
     constructor(props) {
         super(props);
-
+        this.state={
+            id:0,
+            points:[
+                {
+                    id:0,
+                    keyword:'',
+                    status:'new',
+                    children:[
+                        {
+                            id:1
+                        }
+                    ]
+                }
+            ]
+        }
     }
     render() {
         return(
             <div className="container">
-                
+                <hr/>
+                {this.state.points.map((Item,outsideIndex)=>{
+                    return(
+                        <Card
+                            key={outsideIndex}
+                            className={"Collector"}
+                            title={
+                                <Row justify={"center"} align={"middle"} wrap={false} style={{padding:"5px"}}>
+                                    <Col span={20}>
+                                        <textarea
+                                            style={{width:"90%"}}
+                                        />
+                                    </Col>
+                                    <Col span={4}>
+                                        <Row justify={"start"} align={"middle"}>
+                                            <Select>
+                                                <Option value={"new"}>New</Option>
+                                                <Option value={"solved"}>Solved</Option>
+                                                <Option value={"give_up"}>Give Up</Option>
+                                            </Select>
+                                        </Row>
+                                        <Row style={{paddingTop:"4px",paddingBottom:"4px"}} justify={"start"} align={"middle"}>
+                                            <SaveOutlined/>
+                                        </Row>
+                                        <Row justify={"start"} align={"middle"}>
+                                            <PlusCircleOutlined/>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                            }
+                        >
+                            {Item.children.map((childItem,index)=>{
+                                return(
+                                    <Point
+                                        key={index}
+                                        pid={Item.id}
+                                        id={childItem.id}
+                                    />
+                                )
+                            })}
+                        </Card>
+                    )
+                })}
             </div>
         )
     }
