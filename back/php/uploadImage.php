6e9d052f90225126eb7e37c6cd2f859cc4e80e97
Adam <qingxianyang@gmail.com>
2021-09-26 18:24:33 +0800

    设置图片上传功能

---
+++ back/php/uploadImage.php
@@ -0,0 +1,23 @@
+<?php
+
+$tmp = $_FILES['myfile']['tmp_name'];
+
+$filepath = __DIR__.DIRECTORY_SEPARATOR.'photo'.DIRECTORY_SEPARATOR;
+$storeFileName=time().".jpg";
+
+if (move_uploaded_file($tmp, $filepath . $storeFileName)) {
+    echo json_encode($storeFileName);
+} else {
+    echo json_encode($storeFileName,false);
+}
+
+
+function returnData($fileName,$uploadResult=true){
+    return [
+        'Data'=>[
+            'url'=>$fileName,
+        ],
+        'Status'=>$uploadResult?1:0,
+        'Message'=>$uploadResult?"上传成功":"上传失败"
+    ];
+}
